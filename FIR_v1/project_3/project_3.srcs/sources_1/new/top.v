a8a21d7f41208570948dc52d2bd00ee04016c0f8
Gabriele Balbi <gabriele.balbi@gmail.com>
2024-03-15 22:01:56 +0100

    Filtro a 16 taps

--- FIR_v1/project_3/project_3.srcs/sources_1/new/top.v
+++ FIR_v1/project_3/project_3.srcs/sources_1/new/top.v
@@ -32,6 +32,15 @@ wire signed [19:0] mtap4;
 wire signed [19:0] mtap5;
 wire signed [19:0] mtap6;
 wire signed [19:0] mtap7;
+wire signed [19:0] mtap8;
+wire signed [19:0] mtap9;
+wire signed [19:0] mtap10;
+wire signed [19:0] mtap11;
+wire signed [19:0] mtap12;
+wire signed [19:0] mtap13;
+wire signed [19:0] mtap14;
+wire signed [19:0] mtap15;
+
 
 reg signed [19:0] macc0;
 reg signed [19:0] macc1;
@@ -41,6 +50,16 @@ reg signed [19:0] macc4;
 reg signed [19:0] macc5;
 reg signed [19:0] macc6;
 reg signed [19:0] macc7;
+reg signed [19:0] macc8;
+reg signed [19:0] macc9;
+reg signed [19:0] macc10;
+reg signed [19:0] macc11;
+reg signed [19:0] macc12;
+reg signed [19:0] macc13;
+reg signed [19:0] macc14;
+reg signed [19:0] macc15;
+
+
 
 reg signed [19:0] mbuf0;
 reg signed [19:0] mbuf1;
@@ -50,6 +69,14 @@ reg signed [19:0] mbuf4;
 reg signed [19:0] mbuf5;
 reg signed [19:0] mbuf6;
 reg signed [19:0] mbuf7;
+reg signed [19:0] mbuf8;
+reg signed [19:0] mbuf9;
+reg signed [19:0] mbuf10;
+reg signed [19:0] mbuf11;
+reg signed [19:0] mbuf12;
+reg signed [19:0] mbuf13;
+reg signed [19:0] mbuf14;
+reg signed [19:0] mbuf15;
 
 
 
@@ -85,14 +112,22 @@ end
 
 
 
-    assign mtap0 = 20'h00001;  // 
+		assign mtap0 = 20'h00001;  // 
    	assign mtap1 = 20'h00001;  // 
    	assign mtap2 = 20'h00001;  // 
    	assign mtap3 = 20'h00001;  // 
    	assign mtap4 = 20'h00001; 
    	assign mtap5 = 20'h00001; 
    	assign mtap6 = 20'h00001; 
-   	assign mtap7 = 20'h00001; 
+   	assign mtap7 = 20'h00001;
+		assign mtap8 = 20'h00001;  // 
+   	assign mtap9 = 20'h00001;  // 
+		assign mtap10 = 20'h00001;  // 
+   	assign mtap11 = 20'h00001;  // 
+   	assign mtap12 = 20'h00001;  // 
+   	assign mtap13 = 20'h00001;  // 
+   	assign mtap14 = 20'h00001; 
+   	assign mtap15 = 20'h00001; 
    	   	   	   	   	
 		
 		always @ (posedge clk)
@@ -109,7 +144,15 @@ end
    	                    mbuf4 <= mbuf3;
    	                    mbuf5 <= mbuf4;
    	                    mbuf6 <= mbuf5;
-   	                    mbuf7 <= mbuf6;      
+   	                    mbuf7 <= mbuf6;
+								  mbuf8 <=  mbuf7;
+								  mbuf9 <=  mbuf8;
+								  mbuf10 <= mbuf9;
+								  mbuf11 <= mbuf10;
+								  mbuf12 <= mbuf11;
+								  mbuf13 <= mbuf12;
+								  mbuf14 <= mbuf13;
+								  mbuf15 <= mbuf14;
    	                end
    	            else
    	                begin
@@ -120,7 +163,17 @@ end
    	                    mbuf4 <= mbuf4;
    	                    mbuf5 <= mbuf5;
    	                    mbuf6 <= mbuf6;
-   	                    mbuf7 <= mbuf7;            
+   	                    mbuf7 <=  mbuf7;
+								  mbuf8 <=  mbuf8;
+								  mbuf9 <=  mbuf9;
+								  mbuf10 <= mbuf10;
+								  mbuf11 <= mbuf11;
+								  mbuf12 <= mbuf12;
+								  mbuf13 <= mbuf13;
+								  mbuf14 <= mbuf14;
+								  mbuf15 <= mbuf15;
+								  
+								  
    	                end
    	        end
 		
@@ -136,7 +189,18 @@ end
    	                           macc4 <= mtap4 * mbuf4;
    	                           macc5 <= mtap5 * mbuf5;
    	                           macc6 <= mtap6 * mbuf6;
-   	                           macc7 <= mtap7 * mbuf7;  	                              	                              	                           macc4 <= mtap4 * mbuf4; 
+   	                           macc7 <= mtap7 * mbuf7;
+											macc8 <= mtap8 * mbuf8;
+ 											macc9 <= mtap9 * mbuf9;											
+											macc10 <= mtap10 * mbuf10;
+											macc11 <= mtap11 * mbuf11;
+											macc12 <= mtap12 * mbuf12;
+											macc13 <= mtap13 * mbuf13;
+											macc14 <= mtap14 * mbuf14;
+											macc15 <= mtap15 * mbuf15;
+									
+											
+
    	                       end
    	               end    
    	               
@@ -145,7 +209,7 @@ end
    	               begin
    	                   if (btn0 == 1'b1)
    	                       begin
-   	                            total_sins20_filtered <=( macc0 + macc1 + macc2 + macc3+ macc4 + macc5 + macc6 + macc7)/8;
+   	                            total_sins20_filtered <=( macc0 + macc1 + macc2 + macc3+ macc4 + macc5 + macc6 + macc7 + macc8 + macc9 + macc10 + macc11 + macc12 + macc13+ macc14 + macc15)/16;
    	                            signal_out <= total_sins20_filtered;
    	                       end
    	                   else
@@ -156,7 +220,7 @@ end
 
 
 
-    
+     
     
     
 endmodule
